0126a489e19537e408d0dfc62d0a11828d9b11bd
UĞUR <yalinugur00@outlook.com>
2025-03-16 13:20:56 +0300

    Update ex008.py

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,2 +1,9 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543014
+# Your Name and Surname:Uğur Yalın
+number = (int)(input("Enter a number: "))
+for i in range(number):
+    for j in range(number - i - 1):        
+        print(" ",end="")
+    for j in range(2*i + 1):
+        print("*",end="")
+    print()
