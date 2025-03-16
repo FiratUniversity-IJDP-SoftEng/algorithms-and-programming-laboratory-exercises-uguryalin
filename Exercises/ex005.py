4f0f10e496cdf74e352ed8534eefd5ca7804e6e9
UĞUR <yalinugur00@outlook.com>
2025-03-16 13:18:54 +0300

    Update ex005.py

--- Exercises/ex005.py
+++ Exercises/ex005.py
@@ -1,2 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543014
+# Your Name and Surname:Uğur Yalın
+firtsname = str (input("Please enter your first name")) 
+lastname = str (input("Please enter your last name"))
+print (f"Hi\t{firtsname}\n  {lastname}")
