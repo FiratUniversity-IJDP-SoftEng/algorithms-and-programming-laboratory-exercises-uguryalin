00e741aa72a12ff8b35cba03fb36722d91b29302
UĞUR <yalinugur00@outlook.com>
2025-03-16 13:22:00 +0300

    Update ex010.py

--- Exercises/ex010.py
+++ Exercises/ex010.py
@@ -1,2 +1,38 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543014
+# Your Name and Surname:Uğur Yalın
+while True:
+  operation = input(
+      "which method do yopu want use ?\n please enter a for addition\n please enter s for subtraction\n please enter m for multiplication\n please enter d for division\n"
+  ).lower()
+
+  if operation == "a":
+    num1 = int(input("please enter the firts number : "))
+    num2 = int(input("please enter the second number : "))
+    result = num1 + num2
+    print(f"total is : {result}")
+
+  elif operation == "s":
+    num1 = int(input("please enter the firts number : "))
+    num2 = int(input("please enter the second number : "))
+    result = num1 - num2
+    print(f"total is : {result}")
+  elif operation == "m":
+    num1 = int(input("please enter the firts number : "))
+    num2 = int(input("please enter the second number : "))
+    result = num1 * num2
+    print(f"total is : {result}")
+  elif operation == "d":
+    num1 = int(input("please enter the firts number : "))
+    num2 = int(input("please enter the second number but it can not be 0 : "))
+    if num2 == 0:
+      print("you can not divide by 0")
+      break
+    result = float(num1 / num2)
+    print(f"total is : {result}")
+  else:
+    print("please enter a valid input")
+  newOperation = input(
+      "Do you want to do another operation ? [yes/ no] : ").lower()
+  if newOperation != "yes":
+    print("Exitting program.")
+    break
