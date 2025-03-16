bf0b5ed219217c2d51dc060ab5a4ecc4a603b7d7
UĞUR <yalinugur00@outlook.com>
2025-03-16 13:19:30 +0300

    Update ex006.py

--- Exercises/ex006.py
+++ Exercises/ex006.py
@@ -1,2 +1,24 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543014
+# Your Name and Surname:Uğur Yalın
+myList = [1, 3, 9, 2, 9, 5, 4, 10, 3, 6]
+print("Original list is:\n", myList)
+
+reversedList = myList[::-1]  
+print("reversed list:", reversedList)
+
+print("Numbers will add")
+addNumList = [12, 11, 13]
+reversedList.extend(addNumList)  
+print(reversedList)
+
+
+length = len(reversedList)
+print(f"The length of the list is {length}")
+
+print("Removing numbers")
+reversedList.pop(0)  
+reversedList.pop(-1) 
+print(reversedList)
+
+even_numbers = sorted([num for num in reversedList if num % 2 == 0])
+print("Sorted even numbers:", even_numbers)
