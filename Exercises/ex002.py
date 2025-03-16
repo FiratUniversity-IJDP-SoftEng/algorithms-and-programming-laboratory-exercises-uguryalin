6c13d36869bcb4e51fb3e296f3a4225300004090
UĞUR <yalinugur00@outlook.com>
2025-03-16 13:17:02 +0300

    Update ex002.py

--- Exercises/ex002.py
+++ Exercises/ex002.py
@@ -1,2 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543014
+# Your Name and Surname:Uğur Yalın
+import sys
+print("Python version:\n", sys.version)
+print("Version info:\n", sys.version_info)
